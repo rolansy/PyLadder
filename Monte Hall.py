568364da44ca725bccb9a9e1a81b3eb3a1634220
rolansy <ronalshoeygeorge@gmail.com>
2023-10-24 23:33:37 +0530

    without player swap

--- Monte Hall.py
+++ Monte Hall.py
@@ -15,12 +15,19 @@ c=int(input("Enter your Choice : "))
 dooropen=random.choice(gdoor)
 while(dooropen==c): #so that at the end, dooropen != c
     dooropen=random.choice(gdoor)
-ch=input("Do you want to swap : ")
+print("Open Door : Goat : ",dooropen)
+ch=input("Do you want to swap [y/n]: ")
 if ch=='y':
     if door[c]=="Goat":
         print("Player Won")
         swap+=1
     else:
         print("Player Lost")
+else:
+    if door[c]=="Goat":
+        print("Player Lost")
+    else:
+        print("Player Lost")
+        dontswap+=1
     
 
