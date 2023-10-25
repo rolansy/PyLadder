5a91dbd9c2e6894cf4a0f69ad945ba924dc6a0fc
rolansy <ronalshoeygeorge@gmail.com>
2023-10-25 22:22:08 +0530

    Array correction and Function made

--- Binary Search.py
+++ Binary Search.py
@@ -1,5 +1,23 @@
 import random
 
 a=[]
-for i in range(1,1000):
-    a.append(random.randint(1,100))
+for i in range(1,100):
+    x=random.randint(500,1000)
+    if x not in a:
+        a.append(random.randint(1,10000))
+
+def bins(a,x):
+    top=0
+    end=len(a)-1
+    f=0
+    while(top<end and f==0):
+        mid=(top+end)//2
+        if x==a[mid]:
+            f=1
+            print("Element Found At : ",mid)
+            return
+        elif x<a[mid]:
+            end=mid-1
+        else: 
+            top=mid+1
+    
