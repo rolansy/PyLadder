afbfab8c18d14983ba0cb383ea82b806ec81ecbb
rolansy <ronalshoeygeorge@gmail.com>
2023-10-25 21:27:23 +0530

    done bubble sort

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,7 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 25 20:57:44 2023
-
-@author: ronal
-"""
-
+def bubble(a):
+    n=len(a)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if a[j]>a[j+1]:
+                temp=a[j]
+                a[j]=a[j+1]
+                a[j+1]=temp
+    return a
+a=[3,8,5,2,1,10,7,6,4]
+print(bubble(a))
