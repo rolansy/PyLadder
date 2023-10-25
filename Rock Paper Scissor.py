08b6d03a1925807c6e8c317adc7baa79bee8645d
rolansy <ronalshoeygeorge@gmail.com>
2023-10-25 20:31:45 +0530

    ROck Paper Scissor algorithm

--- Rock Paper Scissor.py
+++ Rock Paper Scissor.py
@@ -1,17 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 25 18:53:21 2023
-
-@author: ronal
-"""
-
-p1={0:'Rock',1:'Paper',2:'Scissor'}
-p2={0:'Paper',1:'Rock',2:'Scissor'}
+def rps(n1,n2,b1,b2):
+    p1=int(n1[b1])%3
+    p2=int(n2[b2])%3
+    if pl1[p1]==pl2[p2]:
+        print("Draw")
+    elif pl1[p1]=='r' and pl2[p2]=='s':
+        print("Player 1 Wins")
+    elif pl1[p1]=='p' and pl2[p2]=='r':
+        print("Player 1 Wins")
+    elif pl1[p1]=='s' and pl2[p2]=='p':
+        print("Player 1 Wins")
+    elif pl1[p1]=='r' and pl2[p2]=='s':
+        print("Player 2 Wins")
+    elif pl1[p1]=='r' and pl2[p2]=='p':
+        print("Player 2 Wins")
+    elif pl1[p1]=='p' and pl2[p2]=='s':
+        print("Player 2 Wins")    
+    
+pl1={0:'r',1:'p',2:'s'}
+pl2={0:'p',1:'r',2:'s'}
 while(1):
     n1=input("Player 1 Enter Choice : ")
     n1=input("Player 2 Enter Choice : ")
-    bit=int(input("Player , Enter Secret Bit Position : "))
-    bit=int(input("Player , Enter Secret Bit Position : "))
+    bit1=int(input("Player , Enter Secret Bit Position : "))
+    bit2=int(input("Player , Enter Secret Bit Position : "))
     ch=input("Do you Want to Make Changes : ")
     if (ch=='n'):
         break
