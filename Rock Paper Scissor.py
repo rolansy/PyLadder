ec35c994ff6a4570aeb6277260616e02e7d5ba6e
rolansy <ronalshoeygeorge@gmail.com>
2023-10-25 20:16:33 +0530

    Input Choices

--- Rock Paper Scissor.py
+++ Rock Paper Scissor.py
@@ -7,3 +7,11 @@ Created on Wed Oct 25 18:53:21 2023
 
 p1={0:'Rock',1:'Paper',2:'Scissor'}
 p2={0:'Paper',1:'Rock',2:'Scissor'}
+while(1):
+    n1=input("Player 1 Enter Choice : ")
+    n1=input("Player 2 Enter Choice : ")
+    bit=int(input("Player , Enter Secret Bit Position : "))
+    bit=int(input("Player , Enter Secret Bit Position : "))
+    ch=input("Do you Want to Make Changes : ")
+    if (ch=='n'):
+        break
