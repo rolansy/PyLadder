61530c2248b91af239bff86e069f8d8ec9019cdb
rolansy <ronalshoeygeorge@gmail.com>
2023-10-25 22:01:53 +0530

    Upgrade

--- Linear Search.py
+++ Linear Search.py
@@ -1,8 +1,18 @@
+import random
 def lins(a,x):
+    f=0
+    c=0
     for i in a:
         if i==x:
+            c+=1
             print("Found",i,"at position : ",x)
+            f=1
+    if f==1:
+        print("Number of Occurunces : ",c)
+    else:
+        print("Not Found")
 
-a=list(range(1,1000))
-
-lins(a,500)
+a=[]
+for i in range(1,1000):
+    a.append(random.randint(1,100))
+lins(a,56)
