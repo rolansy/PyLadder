41e2613e2da317dd8cc43c166d887a65bfc15205
rolansy <ronalshoeygeorge@gmail.com>
2023-10-25 22:52:39 +0530

    Show number of iterations

--- Binary Search.py
+++ Binary Search.py
@@ -1,21 +1,22 @@
-import random
 
-a=list(range(1,300))
+a=list(range(1,30000))
 
 def bins(a,x):
     top=0
     end=len(a)-1
     f=0
+    c=0
     while(top<end and f==0):
+        c+=1
         mid=(top+end)//2
         if x==a[mid]:
             f=1
             print("Element Found At : ",mid)
+            print("The Number of Iterations are : ",c)
             return
         elif x<a[mid]:
             end=mid-1
         else: 
             top=mid+1
     print("Not Found")
-bins(a, 250)
-    
+bins(a, 25)
