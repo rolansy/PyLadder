562be06203c98c6d49d8998af0d507ed86035f5e
rolansy <ronalshoeygeorge@gmail.com>
2023-10-28 04:03:05 +0530

    read csv

--- GPS.py
+++ GPS.py
@@ -5,3 +5,14 @@ Created on Sat Oct 28 03:52:25 2023
 @author: ronal
 """
 
+import csv
+with open ('route.csv','r') as f:
+    rr=csv.reader(f)
+    
+    for r in rr:
+        lat=float(r[0])
+        long=float(r[1])
+        print(lat)
+        print(long)
+        print(lat+long)
+        
