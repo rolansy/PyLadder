cdc39a9d62fda5e5ca794af75fe47d0c975f2479
rolansy <ronalshoeygeorge@gmail.com>
2023-10-28 04:51:21 +0530

    matplotlib show graph of profit or loss

--- Gambling Bet.py
+++ Gambling Bet.py
@@ -6,13 +6,20 @@ Created on Sat Oct 28 04:46:18 2023
 """
 
 import random
+import matplotlib.pyplot as plt
 
 acc=100
+x=[]
+y=[]
 for i in range(365):
+    x.append(i+1)
     bet=random.randint(1,10)
     draw=random.randint(1,10)
     if bet==draw:
         acc+=900-100
     else:
         acc-=100
+    y.append(acc)
 print(acc)
+plt.plot(x,y)
+plt.show()
