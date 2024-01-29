2a2bcf946a1f2e4b61fdd52dcdbe0bc16dbc4f96
rolansy <ronalshoeygeorge@gmail.com>
2024-01-30 01:17:14 +0530

    better also: for lopp reverse from min and return whats first cus iukuk

--- Programming, Data Structures And Algorithms but its PYTHONNN/GCD.py
+++ Programming, Data Structures And Algorithms but its PYTHONNN/GCD.py
@@ -1,9 +1,8 @@
 m=int(input("Enter First Number : "))
 n=int(input("Enter Second Number : "))
 def gcd(m,n):
-    for i in range(1,min(m,n)+1):
+    for i in range(min(m,n),0,-1):
         if m%i==0 and n%i==0:
-            xgcd=i
-    return (xgcd)
-        
+            return(i)
+
 print("GCD of ",m," and ",n," is : ",gcd(m,n))
