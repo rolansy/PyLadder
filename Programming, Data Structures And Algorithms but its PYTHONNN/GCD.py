c22be68cc5753873116fe4153b3be5dcdda82015
rolansy <ronalshoeygeorge@gmail.com>
2024-01-30 01:11:08 +0530

    better algo: no array sikee

--- Programming, Data Structures And Algorithms but its PYTHONNN/GCD.py
+++ Programming, Data Structures And Algorithms but its PYTHONNN/GCD.py
@@ -1,10 +1,9 @@
 m=int(input("Enter First Number : "))
 n=int(input("Enter Second Number : "))
 def gcd(m,n):
-    x=[]
     for i in range(1,min(m,n)+1):
         if m%i==0 and n%i==0:
-            x.append(i)
-    return (x[-1])
+            xgcd=i
+    return (xgcd)
         
 print("GCD of ",m," and ",n," is : ",gcd(m,n))
