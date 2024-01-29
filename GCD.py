de79e3061d965e7bb95e480f9a133425070bf402
rolansy <ronalshoeygeorge@gmail.com>
2024-01-30 00:57:42 +0530

    Refactored GCD calculation logic

--- GCD.py
+++ GCD.py
@@ -1,16 +1,18 @@
 m=int(input("Enter First Number : "))
 n=int(input("Enter Second Number : "))
-a=[]
-b=[]
-for i in range(1,m+1):
-    if m%i==0:
-        a.append(i)
-for i in range(1,n+1):
-    if n%i==0:
-        b.append(i)
-x=[]
-for i in a:
-    if i in b:
-        x.append(i)
+def gcd(m,n):
+    a=[]
+    b=[]
+    for i in range(1,m+1):
+        if m%i==0:
+            a.append(i)
+    for i in range(1,n+1):
+        if n%i==0:
+            b.append(i)
+    x=[]
+    for i in a:
+        if i in b:
+            x.append(i)
+    return (x[-1])
         
-print("GCD of ",m," and ",n," is : ",max(x))
+print("GCD of ",m," and ",n," is : ",gcd(m,n))
