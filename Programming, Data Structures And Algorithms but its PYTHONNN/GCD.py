405a2864372df6d62deb9e36a7646b7b4e3c5688
rolansy <ronalshoeygeorge@gmail.com>
2024-01-30 01:06:27 +0530

    better algorithm: using max instead of two for loops

--- Programming, Data Structures And Algorithms but its PYTHONNN/GCD.py
+++ Programming, Data Structures And Algorithms but its PYTHONNN/GCD.py
@@ -3,10 +3,9 @@ n=int(input("Enter Second Number : "))
 def gcd(m,n):
     a=[]
     b=[]
-    for i in range(1,m+1):
+    for i in range(1,max(m,n)+1):
         if m%i==0:
             a.append(i)
-    for i in range(1,n+1):
         if n%i==0:
             b.append(i)
     x=[]
