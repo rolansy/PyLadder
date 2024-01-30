f2276d2ecaf22c8b635f20d604a08c5b6cd2ba47
rolansy <ronalshoeygeorge@gmail.com>
2024-01-31 00:23:59 +0530

    Add Euclid's algorithm for GCD calculation

--- Programming, Data Structures And Algorithms but its PYTHONNN/Euclid's algo GCD.py
+++ Programming, Data Structures And Algorithms but its PYTHONNN/Euclid's algo GCD.py
@@ -13,7 +13,23 @@ def gcd(m,n):
         d=m-n
         m,n=max(n,d),min(n,d)
     return n
+
+def euclidgcdr(m,n):
+    m,n=max(m,n),min(m,n)
+    if m%n==0:
+        return n
+    else:
+        return (gcdr(n,m%n)) #m%n=remainder
+def euclidgcd(m,n):
+    m,n=max(m,n),min(m,n)
+    while m%n!=0:
+        m,n=n,m%n
+    return n
+    
+    
 m=int(input("Enter First Number : "))
 n=int(input("Enter Second Number : "))
 print("GCD with recursion : ",gcdr(m,n))
 print("GCD with while (no recursion) : ",gcd(m,n))
+print("Euclid's Algo GCD with recursion : ",euclidgcdr(m, n))
+print("Euclid's Algo GCD with while (no recursion) : ",euclidgcdr(m, n))
