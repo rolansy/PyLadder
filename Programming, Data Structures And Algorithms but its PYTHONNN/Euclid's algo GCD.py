bc1d0c46d728c1b614ee8c289f104ae1ad3f40c1
rolansy <ronalshoeygeorge@gmail.com>
2024-01-31 00:08:33 +0530

    Refactor Euclid's GCD algorithm to use without recursion and add a while loop version

--- Programming, Data Structures And Algorithms but its PYTHONNN/Euclid's algo GCD.py
+++ Programming, Data Structures And Algorithms but its PYTHONNN/Euclid's algo GCD.py
@@ -1,11 +1,19 @@
-def gcd(m,n):
+def gcdr(m,n): #using recursion
     m,n=max(m,n),min(m,n)
     
     if m%n==0:
         return n
     else:
         d=m-n
-        return(gcd(max(n,d),min(n,d)))
+        return(gcdr(max(n,d),min(n,d)))
+    
+def gcd(m,n):
+    m,n=max(m,n),min(m,n)
+    while m%n!=0:
+        d=m-n
+        m,n=max(n,d),min(n,d)
+    return n
 m=int(input("Enter First Number : "))
 n=int(input("Enter Second Number : "))
-print("GCD : ",gcd(m,n))
+print("GCD with recursion : ",gcdr(m,n))
+print("GCD with while (no recursion) : ",gcd(m,n))
