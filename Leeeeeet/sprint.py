9714f80d01f9a6671665a11ff0243c31ecfe9139
rolansy <ronalshoeygeorge@gmail.com>
2024-03-06 02:07:09 +0530

    Refactor minimumLength function to remove unnecessary code and improve readability

--- Leeeeeet/sprint.py
+++ Leeeeeet/sprint.py
@@ -3,16 +3,10 @@ def minimumLength( s):
         :type s: str
         :rtype: int
         """
-        while True:
-            if s=="":
-                return 0
-            x=s[0]
-            if s.lstrip(x)!=s and s.rstrip(x)!=s:
-                s=s.strip(x)
-                continue
-            else:
-                return len(s)
+        while len(s)>1 and s[0]==s[-1]:
+            s=s.strip(s[0])
+        return len(s)
         
         
-s="ca"
+s="aabccabba"
 print(minimumLength(s))
