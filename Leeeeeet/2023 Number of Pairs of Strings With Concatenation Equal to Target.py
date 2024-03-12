3f6eb1cf871da4e230a7761a93f387c8afd24dcc
rolansy <ronalshoeygeorge@gmail.com>
2024-03-13 02:36:24 +0530

    Add solution for Number of Pairs of Strings With Concatenation Equal to Target problem

---
+++ Leeeeeet/2023 Number of Pairs of Strings With Concatenation Equal to Target.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 02:34:20 2024
+
+@author: ronal
+"""
+
+def numOfPairs( nums, target):
+        """
+        :type nums: List[str]
+        :type target: str
+        :rtype: int
+        """
+        s=0
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i!=j and nums[i]+nums[j]==target:
+                    s+=1
+        return s
+nums=["777","7","77","77"]
+target="7777"
+#4
+print(numOfPairs(nums, target))
