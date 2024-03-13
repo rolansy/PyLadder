591a782ba0569eedb44ff708902dbe7b23ffa7b4
rolansy <ronalshoeygeorge@gmail.com>
2024-03-14 02:57:31 +0530

    reduce time complx

--- Leeeeeet/2485 Find the Pivot Integer.py
+++ Leeeeeet/2485 Find the Pivot Integer.py
@@ -1,8 +1,9 @@
 def pivotInteger(n):
+    if n==1:
+        return 1
     l=list(range(1,n+1))
     for i in range(1,n+1):
         if sum(l[:i])==sum(l[i-1:]):
             return i
-    return -1
     return -1
 print(pivotInteger(8))
