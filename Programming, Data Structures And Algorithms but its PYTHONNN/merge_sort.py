2239f01ace62155f2da8658314f2fb1cf98c1ec7
rolansy <ronalshoeygeorge@gmail.com>
2024-03-20 16:11:28 +0530

    alternatives

--- Programming, Data Structures And Algorithms but its PYTHONNN/merge_sort.py
+++ Programming, Data Structures And Algorithms but its PYTHONNN/merge_sort.py
@@ -4,11 +4,15 @@ def merge(a,b):
     i,j=0,0
     while i+j<m+n:
         if i==m:
-            c.append(b[j])
-            j+=1
+            c+=b[j:]
+            j+=len(b)-j
+            #c.append(b[j])
+            #j+=1
         elif j==n:
-            c.append(b[i])
-            i+=1
+            c+=a[i:]
+            j+=len(a)-i
+            #c.append(b[i])
+            #i+=1
         elif a[i]<=b[j]:
             c.append(a[i])
             i+=1
