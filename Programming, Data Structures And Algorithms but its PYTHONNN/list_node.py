34ec8f8160e961ce820c6d6ab68520cec7589d66
rolansy <ronalshoeygeorge@gmail.com>
2024-03-24 01:20:02 +0530

    Add delete and deleter methods to ListNode class

--- Programming, Data Structures And Algorithms but its PYTHONNN/list_node.py
+++ Programming, Data Structures And Algorithms but its PYTHONNN/list_node.py
@@ -54,8 +54,59 @@ class Node:
         self.next,newnode.next=newnode,self.next
         return
     
-    
+    def delete(self,v):
+        if self.isempty():
+            print("Empty")
+            return
+        if self.value==v:
+            if self.next==None:
+                self.value=None
+                return
+            else:
+                self.value=self.next.value
+                self.next=self.next.next
+                return
+                
+        temp=self
+        while temp.next!=None:
+            if temp.next.value==v:
+                temp.next=temp.next.next
+                return
+            else:
+                temp=temp.next
         
+        return
+    
+    def deleter(self,v):
+        if self.isempty():
+            print("Empty")
+            return
+        if self.value==v:
+            if self.next==None:
+                self.value=None
+                return
+            else:
+                self.value=self.next.value
+                self.next=self.next.next
+                return
+        
+        if self.next!=None:
+            self.next.deleter(v)
+            if self.next.value==None:
+                self.next=self.next.next
+        return
+        
+    def __str__(self):
+        selflist=[]
+        
+        if self.value==None:
+            return(str(selflist))
+        temp=self
+        selflist.append(temp.value)
+        while temp.next!=None:
+            temp=temp.next
+            selflist.append(temp.value)
+        return(str(selflist))
     
     
     
