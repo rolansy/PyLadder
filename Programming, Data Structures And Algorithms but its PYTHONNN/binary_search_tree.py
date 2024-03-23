cc8920e28b67f8ea9afa627377a8c067b5cb5695
rolansy <ronalshoeygeorge@gmail.com>
2024-03-24 02:27:16 +0530

    Add binary search tree implementation in Python

---
+++ Programming, Data Structures And Algorithms but its PYTHONNN/binary_search_tree.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 24 02:08:22 2024
+
+@author: ronal
+"""
+
+class tree:
+    def __init__(self,initval=None):
+        self.value=initval
+        if self.value:
+            self.left=tree(input("Enter Left Node : "))
+            self.right=tree(input("Enter Right Node : "))
+        else:
+            self.left=None
+            self.right=None
+        return
+    
+    def isempty(self):
+        return self.value==None
+    
+    def inorder(self):
+        if self.value==None:
+            return([])
+        if self.isempty():
+            return([])
+        return(self.left.inorder()+[self.value]+self.right.inorder())
+        
+    def __str__(self):
+        return(str(self.inorder()))
+    
+    def find(self,v):
+        if self.isempty():
+            return False
+        if self.value==v:
+            return True
+        if v<self.value:
+            return self.left.find(v)
+        return self.right.find(v)
+    
+    def minval(self):
+        if self.left==None:
+            return(self.value)
+            
+        return self.left.minval()
+    
+    def maxval(self):
+        if self.right==None:
+            return (self.value)
+        return self.right.maxval()
+    
+    
+    
+    
