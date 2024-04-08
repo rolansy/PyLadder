7667785919a169eb4c0f062b08568672b87bcff6
rolansy <ronalshoeygeorge@gmail.com>
2024-04-09 02:50:32 +0530

    Add dynamic programming implementation of Fibonacci sequence

--- Data Science pyyyy/memoized_fibonacci.py
+++ Data Science pyyyy/memoized_fibonacci.py
@@ -16,7 +16,7 @@ def mem_fib(n):
     fibtable[n]=value
     print(value)
     return value
-
+ 
 def fib(n):
     if n in [0,1]:
         value=n
@@ -24,3 +24,10 @@ def fib(n):
         value=fib(n-1)+fib(n-2)
     print(value)
     return value
+
+def dyn_fib(n):
+    fibtable[0]=0
+    fibtable[1]=1
+    for i in range(2,n+1):
+        fibtable[i]=fibtable[i-1]+fibtable[i-2]
+    return(fibtable[n])
