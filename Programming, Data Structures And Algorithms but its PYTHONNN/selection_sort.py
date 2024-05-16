9028771a0442d7be73a6ba1fbdd431189e523dda
rolansy <ronalshoeygeorge@gmail.com>
2024-05-16 14:02:20 +0300

    feat: Refactor selection sort algorithm and add test case

--- Programming, Data Structures And Algorithms but its PYTHONNN/selection_sort.py
+++ Programming, Data Structures And Algorithms but its PYTHONNN/selection_sort.py
@@ -4,7 +4,7 @@ Created on Wed Mar 20 11:45:31 2024
 
 @author: ronal
 """
-
+from random import shuffle
 def selectionsort(l):
     for start in range(len(l)):
         minpos=start
@@ -13,6 +13,10 @@ def selectionsort(l):
                 minpos=j
         l[start],l[minpos]=l[minpos],l[start]
     return l
-l=list(map(int,input("Enter List : ").split()))
-print(selectionsort(l))
+#l=list(map(int,input("Enter List : ").split()))
+l=list(range(4,200000,3))
+shuffle(l)
+print(l)
+s=selectionsort(l)
+print(s)
                 
