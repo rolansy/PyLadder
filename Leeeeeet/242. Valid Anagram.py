56e9392a4972481beda4d1c212781ee178d973c0
rolansy <ronalshoeygeorge@gmail.com>
2025-07-03 00:11:02 +0530

    feat: Refactor isAnagram function to include sorting method and dictionary counting method

--- Leeeeeet/242. Valid Anagram.py
+++ Leeeeeet/242. Valid Anagram.py
@@ -1,4 +1,16 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+    # Method 1: Using sorting
+    def isAnagram1(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
     
+    # Method 2: Using dictionary counting
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        counts, countt = {}, {}
+
+        for i in range(len(s)):
+            counts[s[i]] = counts.get(s[i], 0) + 1
+            countt[t[i]] = countt.get(t[i], 0) + 1
+
+        return countt == counts
