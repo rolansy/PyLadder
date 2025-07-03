698e2d1a4ad4249016e738c8765bfba9bb295056
rolansy <ronalshoeygeorge@gmail.com>
2025-07-04 00:08:55 +0530

    refactor: Clean up groupAnagrams function and improve readability

--- Leeeeeet/49. Group Anagrams.py
+++ Leeeeeet/49. Group Anagrams.py
@@ -1,10 +1,22 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        h={}
+        h = {}
         for i in strs:
-            ix="".join(sorted(i))
-            if ix not in list(h.keys()):
-                h[ix]=[i]
+            ix = "".join(sorted(i))
+            if ix not in h:
+                h[ix] = [i]
             else:
                 h[ix].append(i)
-        return list(h.values())      
+        return list(h.values())
+    
+    def groupAnagramsCount(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
